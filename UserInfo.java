7dedcdfbe8a6fd4565812ef2a624e7a6d0a8f38a
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-03-08 21:00:32 +0600

    Add Getter and Setter

---
+++ UserInfo.java
@@ -0,0 +1,45 @@
+public class User {
+    
+    private String name;
+    private String email;
+    private String roll;
+    private String boarder;
+    private String mobileNumber;
+    //private String address;
+    private String securityQuestion;
+    private String answer;
+    private String password;
+    private String status;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    public String getBoarder() {
+        return boarder;
+    }
+
+    public void setBoarder(String boarder) {
+        this.boarder = boarder;
+    }
